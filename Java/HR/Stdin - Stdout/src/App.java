d47265334b98632f3c88c1d5ee1900d3f07367e8
Atari777 <55715664+Atari777@users.noreply.github.com>
2021-09-27 15:25:20 -0500

    mi primer commit

---
+++ Java/HR/Stdin - Stdout/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    
+
+    public static void main(String[] args) throws Exception {
+       
+        /*
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+    
+            // Write your code here.
+        double d = scan.nextDouble();
+        String s = scan.next();
+            
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        */
+    }
+}
