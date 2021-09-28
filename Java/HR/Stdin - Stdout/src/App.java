1e781c09a37f58677454b57c2d935eae684e44c6
Atari777 <55715664+Atari777@users.noreply.github.com>
2021-09-28 14:20:13 -0500

    First Excersice finished

--- Java/HR/Stdin - Stdout/src/App.java
+++ Java/HR/Stdin - Stdout/src/App.java
@@ -4,18 +4,24 @@ public class App {
     
 
     public static void main(String[] args) throws Exception {
-       
-        /*
+
+        // imprime un string un doule y una cadena de texto 
+        
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
-    
-            // Write your code here.
+        System.out.println("Int: " + i);
+        scan.nextLine();
         double d = scan.nextDouble();
-        String s = scan.next();
-            
+        scan.nextLine();
+        String s = scan.nextLine();
+
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
-        */
+        
+        scan.close();
+        
     }
+
+    
 }
