23fcba641c4e8528dd20b6919c571a58af042507
Ariel Marcelo <ariel.pillajo@epn.edu.ec>
2021-12-27 14:12:58 -0500

    comments hill climbing

--- Inteligencia Artificial/Taller 5 -Hill Climbing/hospitals.py
+++ Inteligencia Artificial/Taller 5 -Hill Climbing/hospitals.py
@@ -4,83 +4,92 @@ import random
 class Space():
 
     def __init__(self, height, width, num_hospitals):
-        """Create a new state space with given dimensions."""
+        """Crear un nuevo estado con las dimensiones dadas."""
+        # Definir las dimensiones del espacio y el número de hospitales
         self.height = height
         self.width = width
         self.num_hospitals = num_hospitals
-        self.houses = set()
-        self.hospitals = set()
+        self.houses = set() # Conjunto vácio de casas
+        self.hospitals = set()  # Conjunto vácio de hospitales
 
     def add_house(self, row, col):
-        """Add a house at a particular location in state space."""
+        """Añadir a una casa una locación especifica en el espacio."""
+        # Guardar una casa representada por su posición en el espacio
         self.houses.add((row, col))
 
     def available_spaces(self):
-        """Returns all cells not currently used by a house or hospital."""
+        """Devolver todas las celdas en vacías."""
 
-        # Consider all possible cells
+        # Conjunto de todas las celdas en el espacio en todas las posiciones posibles
         candidates = set(
             (row, col)
             for row in range(self.height)
             for col in range(self.width)
         )
 
-        # Remove all houses and hospitals
+        # Remover todas las casas y hospitales
+        # Elimina cualquier posición ya ocuapada por una casa
         for house in self.houses:
             candidates.remove(house)
         for hospital in self.hospitals:
             candidates.remove(hospital)
         return candidates
 
+    # Hill Climbing normal , se envia log = True para ver el estado de cada iteracion
+    # y el image_prefix para guardar las imagenes de cada iteracion
     def hill_climb(self, maximum=None, image_prefix=None, log=False):
         """Performs hill-climbing to find a solution."""
         count = 0
 
-        # Start by initializing hospitals randomly
+        # la ubicación de los hospitales iniciales son aleatorios
         self.hospitals = set()
         for i in range(self.num_hospitals):
+            # añade posiciones aleatorias para los hospitales
             self.hospitals.add(random.choice(list(self.available_spaces())))
         if log:
+            # Imprime el estado inicial y su costo
             print("Initial state: cost", self.get_cost(self.hospitals))
         if image_prefix:
+            # Genera una imagen del estado inicial
             self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
 
-        # Continue until we reach maximum number of iterations
+        # Repita hasta llegar a un número mázimo de iteraciones o hasta que el estado no cambie
         while maximum is None or count < maximum:
             count += 1
             best_neighbors = []
             best_neighbor_cost = None
 
-            # Consider all hospitals to move
+            # Considere todos los hospitales a mover
             for hospital in self.hospitals:
 
-                # Consider all neighbors for that hospital
+                # Considere todos los vecinos de un hospital que no sean una casa u otro hospital
                 for replacement in self.get_neighbors(*hospital):
+                    
+                    # Generar un conjunto vecino de hospitales
+                    neighbor = self.hospitals.copy() # copia todo el conjunto de hospitales
+                    neighbor.remove(hospital) # remueve el hospital actual que se esta analizando
+                    neighbor.add(replacement) # añade uno de los vecinos de ese hospital
 
-                    # Generate a neighboring set of hospitals
-                    neighbor = self.hospitals.copy()
-                    neighbor.remove(hospital)
-                    neighbor.add(replacement)
-
-                    # Check if neighbor is best so far
-                    cost = self.get_cost(neighbor)
-                    if best_neighbor_cost is None or cost < best_neighbor_cost:
-                        best_neighbor_cost = cost
-                        best_neighbors = [neighbor]
-                    elif best_neighbor_cost == cost:
+                    # Compruebo que vecino es mejor
+                    cost = self.get_cost(neighbor) # costo de la distancia de un vecion al resto de vecinos
+                    if best_neighbor_cost is None or cost < best_neighbor_cost: # si se encuentra un costo más corto
+                        best_neighbor_cost = cost # se actualiza el mejor costo
+                        best_neighbors = [neighbor] # nueva lista de mejores vecinos
+        
+                    elif best_neighbor_cost == cost: # si el costo es igual guarda este vecino en la lista de mejores vecinos
                         best_neighbors.append(neighbor)
 
-            # None of the neighbors are better than the current state
+            # Si ninguno de los vecionos es mejor que el estado actual 
             if best_neighbor_cost >= self.get_cost(self.hospitals):
                 return self.hospitals
 
-            # Move to a highest-valued neighbor
+            # Mudarse a un vecino de mayor valor
             else:
                 if log:
                     print(f"Found better neighbor: cost {best_neighbor_cost}")
                 self.hospitals = random.choice(best_neighbors)
 
-            # Generate image
+            # Generar lmagen
             if image_prefix:
                 self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
 
@@ -89,7 +98,7 @@ class Space():
         best_hospitals = None
         best_cost = None
 
-        # Repeat hill-climbing a fixed number of times
+        # Repite la escalada un número fijo de veces 
         for i in range(maximum):
             hospitals = self.hill_climb()
             cost = self.get_cost(hospitals)
@@ -108,9 +117,10 @@ class Space():
         return best_hospitals
 
     def get_cost(self, hospitals):
-        """Calculates sum of distances from houses to nearest hospital."""
+        """Calcual la suma de distancias desde las casas a los hospitales más cercanos."""
         cost = 0
         for house in self.houses:
+            # Para cada casa se escoje la mínima distancia al comparar con cada hosptial en el espacio
             cost += min(
                 abs(house[0] - hospital[0]) + abs(house[1] - hospital[1])
                 for hospital in hospitals
@@ -118,7 +128,7 @@ class Space():
         return cost
 
     def get_neighbors(self, row, col):
-        """Returns neighbors not already containing a house or hospital."""
+        """Dame la posición de los vecinos q no son una casa u hospital."""
         candidates = [
             (row - 1, col),
             (row + 1, col),
@@ -190,7 +200,7 @@ class Space():
         img.save(filename)
 
 
-# Create a new space and add houses randomly
+# Crear un nuevo espacio y añadir cassas randomicamente
 s = Space(height=10, width=20, num_hospitals=3)
 for i in range(15):
     s.add_house(random.randrange(s.height), random.randrange(s.width))
@@ -199,4 +209,4 @@ for i in range(15):
 hospitals = s.hill_climb(image_prefix="hospitals", log=True)
 
 # Use local search to determine hospital placementhospitals = s.random_restart(20,image_prefix="hospitals", log = True)#
-#hospitals = s.random_restart(20,image_prefix="hospitals", log = True)
+#hospitals = s.random_restart(200,image_prefix="hospitals", log = True)
