19d1cb29367bb1969bfa3d54037b8d7f27455309
Ariel Marcelo <ariel.pillajo@epn.edu.ec>
2022-02-14 15:38:39 -0500

    "act"

--- Inteligencia Artificial/Taller 11-Redes-Bayesianas/inference.py
+++ Inteligencia Artificial/Taller 11-Redes-Bayesianas/inference.py
@@ -2,14 +2,15 @@ from model import model
 
 # Calculate predictions
 predictions = model.predict_proba({
-    "dog": "yes",
+    "dog": "no",
+    "grass": "yes"
 })
 
 # Print predictions for each node
 for node, prediction in zip(model.states, predictions):
     if isinstance(prediction, str):
         print(f"{node.name}: {prediction}")
-    else:
+    elif node.name == "splinkers":
         print(f"{node.name}")
         for value, probability in prediction.parameters[0].items():
-            print(f"    {value}: {probability:.4f} hey")
+            print(f"    {value}: {probability:.4f}")
